7931d6bea286acbf49cca1ffdefe97ab46ead7fd
cniblock <nibbo1984@hotmail.co.uk>
2024-03-19 18:07:52 +0000

    added add task file and function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     MIDIAccess.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialization
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
   });
