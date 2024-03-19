503085c69de3de9603d95d741c3900d99daeb3a5
cniblock <nibbo1984@hotmail.co.uk>
2024-03-19 00:05:37 +0000

    added nav bar and footer

---
+++ .bashrc
@@ -0,0 +1,1 @@
+# Setting locale\nLC_CTYPE=en_US.UTF-8\nLC_ALL=en_US.UTF-8
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    MIDIAccess.Sidenav.init(sidenav);
+  });
